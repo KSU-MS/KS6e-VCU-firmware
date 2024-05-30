8eef6efb2e6844f85746aec7623cc8bff4a17f35
jstri114 <jstri114@students.kennesaw.edu>
2024-05-29 21:21:01 -0400

    wait I forgot the 10x on the torque output

--- src/torque_controller.cpp
+++ src/torque_controller.cpp
@@ -110,7 +110,7 @@ int16_t torque_controllerSlipTime::calculate_torque(unsigned long elapsedTime, i
     }
     
     // Limit torque outputs like normal
-    torqueOut = outputTorqueRTD;
+    torqueOut = outputTorqueRTD * 10; // times 10 because your input is coming from the output of calculate_torque based on the APPS which is Nmx10
     lcTorqueRequest = static_cast<int16_t>(torqueOut); // Pre clamping
     if (torqueOut > maxTorque)
     {
